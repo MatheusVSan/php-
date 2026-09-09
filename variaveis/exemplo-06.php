9168808d50a447cd717a6192566ecd439979824d
MatheusVSantos <mvgemage@gmail.com>
2026-09-09 19:43:48 -0300

    Exercicio adicional

---
+++ variaveis/exemplo-06.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = "teteu";
+
+var_dump ($nome);
+
+echo "<br>";
+
+$nome = "Matheus";
+
+$sobreNome = "Santoss";
+
+$nomeCompleto = $nome . " " . $sobreNome;
+
+var_dump ($nomeCompleto);
+
+?>
