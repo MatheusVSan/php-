86dbe977162439bf60d88bc13e9629bf6afd6a77
MatheusVSantos <mvgemage@gmail.com>
2026-09-11 13:39:00 -0300

    While e DO

---
+++ while/exemplo-01.php
@@ -0,0 +1,15 @@
+<?php
+
+$condicao = true; {
+
+    $numero = rand (1, 10);
+
+    if($numero === 3) {
+
+        echo "TRÊS!!!";
+        $condicao = fase;
+    }
+    echo $numero . " "; 
+}
+
+?>
