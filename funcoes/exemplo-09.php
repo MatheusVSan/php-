c956d3f353f54592518f0b6ed369c063ae7ce9f3
unknown <mvgemage@gmail.com>
2026-09-22 20:58:07 -0300

    funções

---
+++ funcoes/exemplo-09.php
@@ -0,0 +1,56 @@
+<?php
+
+$hierarquia = array(
+
+    array(
+        'nome_cargo' => 'CEO',
+        'subordinados' => array(
+            //Inicio: Diretor Comercial
+            array(
+                'nome_cargo' => 'Diretor Comercial',
+                'subordinados' => array(
+                    //Inicio: Gerente de Vendas
+                    array(
+                        'nome_cargo' => 'Gerente de Vendas'
+                    )
+                    //Termino: Gerente de Vendas
+                )
+            ),
+            //Termino: Diretor Comercial
+            //Inicio: Diretor Financeiro
+            array(
+                'nome_cargo' => 'Diretor Financeiro',
+                'subordinados' => array(
+                    //Inicio: Gerente de Contas a Pagar
+                    array(
+                        'nome_cargo' => 'Gerente de Contas a Pagar',
+                        'subordinados' => array(
+                            //Inicio: Supervisor de Pagamentos
+                            array(
+                                'nome_cargo' => 'Supervisor de Pagamentos'
+                            )
+                            //Termino: Supervisor de Pagamentos
+                        )
+                    ),
+                    //Termino: Gerente de Contas a Pagar
+                    //Inicio: Gerente de Compras
+                    array(
+                        'nome_cargo' => 'Gerente de Compras',
+                        'subordinados' => array(
+                            //Inicio: Supervisor de Suprimentos
+                            array(
+                                'nome_cargo' => 'Supervisor de Suprimentos'
+                            )
+                            //Termino: Supervisor de Suprimentos
+                        )
+                    )
+                    //Termino: Gerente de Compras
+                )
+            )
+            //Termino: Diretor Financeiro
+        )
+    )
+);
+
+fu
+?>
