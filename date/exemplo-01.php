fdb6e7cb1b822be21e26f1e89aebde1ff8b7d2fd
unknown <mvgemage@gmail.com>
2026-09-23 14:23:48 -0300

    DATA

---
+++ date/exemplo-01.php
@@ -0,0 +1,8 @@
+<?php
+
+echo date("d/m/Y H:i:s", 1482758040);
+
+echo "<br>";
+
+echo time();
+?>
---
+++ date/exemplo-02.php
@@ -0,0 +1,7 @@
+<?php
+
+$ts = strtotime("2016-12-25 10:30:00");
+
+echo date("l, d/m/Y H:i:s", $ts);
+
+?>
