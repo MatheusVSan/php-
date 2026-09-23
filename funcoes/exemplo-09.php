2c7a98444fd90d2a0364b1ccb24506ba0ff069c0
unknown <mvgemage@gmail.com>
2026-09-23 14:00:13 -0300

    funcão recursiva

--- funcoes/exemplo-09.php
+++ funcoes/exemplo-09.php
@@ -51,5 +51,23 @@ $hierarquia = array(
         )
     )
 );
-
+ function exibir($cargos) {
+    $html = '<ul>';
+    
+    foreach ($cargos as $cargo) {
+        $html .= '<li>';
+        $html .= $cargo['nome_cargo'];
+        
+        if (isset($cargo['subordinados']) && count($cargo['subordinados']) > 0) {
+            $html .= exibir($cargo['subordinados']);
+        }
+        
+        $html .= '</li>';
+    }
+    
+    $html .= '</ul>';
+    
+    return $html;
+};
+ echo exibir($hierarquia);
 ?>
