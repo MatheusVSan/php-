f420eab5d4e225398b3acb6abd4733c4337302e6
unknown <mvgemage@gmail.com>
2026-09-23 10:40:47 -0300

    Testando sozinho

---
+++ variaveis/exemplo-07.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = "Matheus";
+$idade = 22;
+$altura = 1.80;
+
+    echo "Meu nome é $nome, tenho $idade anos e minha altura é $altura m." . "<br>" ;
+    echo "Nome: $nome<br>";
+    echo "Idade: $idade<br>";
+    echo "Altura: $altura<br>";
+
+?>
